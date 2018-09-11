071fcfb3c9e7a9c1578642e8e4ea88d27f799bbe
Josh Tseng <josh.tseng@hp.com>
2018-09-11 16:17:16 +0800

    Add Demo module.

---
+++ WPFMainUI/Modules/PrismMVVMDemo/PrismMVVMModule.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using PrismMVVMDemo.Views;
+
+namespace PrismMVVMDemo
+{
+    class PrismMVVMModule : IModule
+    {
+        /// <summary>
+        /// The _region manager.
+        /// </summary>
+        private readonly IRegionManager _regionManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuModule"/> class.
+        /// </summary>
+        /// <param name="regionManager">
+        /// The region manager.
+        /// </param>
+        public PrismMVVMModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MenuRegion", typeof(MVVMDemoControl));
+        }
+    }
+}
