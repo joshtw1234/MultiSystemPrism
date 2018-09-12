36bd2c9a3c49382ae9445ab3f73325893bd39f51
Josh Tseng <josh.tseng@hp.com>
2018-09-12 10:39:29 +0800

    Add Bootstrapper.cs Update Region layout.

---
+++ WPFMainUI/WPFMainUI/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Prism.Modularity;
+using Prism.Unity;
+using WPFMainUI.Views;
+
+namespace WPFMainUI
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            //this.Container.RegisterType<SplashScreenModule.IShell, MainWindow>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
